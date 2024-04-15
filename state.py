2b3de9f7c7b881a534db1df2950248e217c61bff
Richard Noh <richard.jun.noh@gmail.com>
2024-04-15 02:21:49 -0500

    Add TetrisPiece and TetrisGrid implementations
    This should give us the base to implement the features

--- state.py
+++ state.py
@@ -1,34 +1,8 @@
-from nes_py.wrappers import JoypadSpace
-import gym_tetris
-from gym_tetris.actions import MOVEMENT
-import matplotlib.pyplot as plt
+# from nes_py.wrappers import JoypadSpace
+# import gym_tetris
+# from gym_tetris.actions import MOVEMENT
+# import matplotlib.pyplot as plt
 import numpy as np
-
-
-env = gym_tetris.make('TetrisA-v3')
-env = JoypadSpace(env, MOVEMENT)
-
-done = True
-for step in range(5000):
-    if done:
-        state = env.reset()
-    state, reward, done, info = env.step(env.action_space.sample())
-# print(env.unwrapped._board)
-
-
-class TetrisGrid:
-    def __init__(self, env):
-        self.env = env.unwrapped # reference to tetris-gym environment
-        self.grid = np.zeros((20, 10), dtype=np.ubyte)
-
-    def get_grid(self):
-        for i in range(20):
-            for j in range(10):
-                self.grid[i, j] = 1 if env.unwrapped._board[i, j] != 0xEF else 0
-        return self.grid
-
-tetris_grid = TetrisGrid(env)
-print(tetris_grid.get_grid())
 
 TETRIS_TILE_POSITIONS = [
     (( -1,  0), (  0,  0), (  1,  0), (  0, -1)),  # 00: T up
@@ -58,15 +32,102 @@ TETRIS_TILE_POSITIONS = [
     (( -2,  0), ( -1,  0), (  0,  0), (  1,  0)),  # 12: I horizontal (spawn)
 ]
 
-# current tetris piece...
+TETRIS_TILE_TYPES = [
+    'Tu',
+    'Tr',
+    'Td',
+    'Tl',
+    'Jl',
+    'Ju',
+    'Jr',
+    'Jd',
+    'Zh',
+    'Zv',
+    'O',
+    'Sh',
+    'Sv',
+    'Lr',
+    'Ld',
+    'Ll',
+    'Lu',
+    'Iv',
+    'Ih'
+]
+
+
+class TetrisGrid:
+    def __init__(self, env):
+        self.env = env.unwrapped # reference to tetris-gym environment
+        self.grid = np.zeros((22, 10), dtype=np.ubyte) # extra 2 rows for the hidden items
+
+    def get_grid(self):
+        for i in range(20):
+            for j in range(10):
+                self.grid[i+2, j] = 1 if self.env._board[i, j] != 0xEF else 0
+        return self.grid[2:, :]
+
+
 class TetrisPiece:
     def __init__(self, env):
         self.env = env.unwrapped
+        self.update()
+
+    def update(self):
+        self.position = (self.env.ram[0x40], self.env.ram[0x41]) 
+        self.type = self.env.ram[0x42]
+
+    def getPieceType(self):
+        self.update()
+        return (self.type, TETRIS_TILE_TYPES[self.type])
+    
+    def getBasePosition(self):
+        self.update()
+        return self.position
+    
+    def getRelativeTilePositions(self):
+        return TETRIS_TILE_POSITIONS[self.type]
+
+    def getAbsoluteTilePositions(self):
+        def add_pos(relative_pos):
+            return tuple(np.add((self.position), relative_pos))
         
+        curr_tilerelpos = TETRIS_TILE_POSITIONS[self.type]
+        return (add_pos(curr_tilerelpos[0]), add_pos(curr_tilerelpos[1]), 
+                    add_pos(curr_tilerelpos[2]), add_pos(curr_tilerelpos[3]))
+    
+    def getGridAndPiece(self, tetrisgrid):
+        self.update()
+        grid_copy = tetrisgrid.grid.copy()
 
-        pass
+        tile_positions = self.getAbsoluteTilePositions()
+        for tile_position in tile_positions:
+            tile_x, tile_y = tile_position
+            grid_copy[tile_y+2, tile_x] = 1
+
+        return grid_copy[2:, :]
 
 
-plt.imshow(state)
-plt.show()
+# env = gym_tetris.make('TetrisA-v3')
+# env = JoypadSpace(env, MOVEMENT)
+
+# done = True
+# for step in range(48*100):
+#     if done:
+#         state = env.reset()
+#     state, reward, done, info = env.step(env.action_space.sample())
+# # print(env.unwrapped._board)
+
+# tetris_grid = TetrisGrid(env)
+# print(tetris_grid.get_grid())
+
+# tetris_piece = TetrisPiece(env)
+# print(tetris_piece.getPieceType())
+# print(tetris_piece.getBasePosition())
+# print(tetris_piece.getRelativeTilePositions())
+# print(tetris_piece.getAbsoluteTilePositions())
+# piece_and_grid = tetris_piece.getGridAndPiece(tetris_grid)
+# print(piece_and_grid)
+
+# plt.imshow(state)
+# plt.show()
 
